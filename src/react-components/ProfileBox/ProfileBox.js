17f599655ffe2443788189b4112e38502376df9b
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2020-03-03 21:22:37 -0500

    Added a default profile picture

--- src/react-components/ProfileBox/ProfileBox.js
+++ src/react-components/ProfileBox/ProfileBox.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import './ProfileBox.css'
+import DEUUEAUGH from "../../data/DEUUEAUGH.png";
+import { withRouter } from 'react-router-dom'
 
-export default class ProfileBox extends Component {
+class ProfileBox extends Component {
     constructor(props) {
         super(props)
-
+        console.log(this.props)
+        console.log(this.props.location.name)
         this.state = {}
     }
 
     render() {
         return (
             <div className="profile-box">
-                <div> Image </div>
-                <div> {this.props.name} </div>
+                <div><strong>{this.props.name}</strong></div>
+                <img className="profile-pic" src={DEUUEAUGH}/>
+
             </div>
         )
     }
 }
+
+export default withRouter(ProfileBox);
