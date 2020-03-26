436a610fc85f3465e3f5a0ddc1e8a98ff9bc40ed
DelmarZhao <delmar.zhao@mail.utoronto.ca>
2020-03-26 17:40:47 -0400

    fix datestring crash

--- client/src/react-components/Calendar/Calendar.js
+++ client/src/react-components/Calendar/Calendar.js
@@ -13,32 +13,26 @@ const localizer = momentLocalizer(moment)
 class ScheduleCalendar extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             events: this.props.events
         }   
     }
 
-    parseDate(dateString){
-        const year = parseInt(dateString.substring(0,4))
-        const month = parseInt(dateString.substring(5,7))
-        const day = parseInt(dateString.substring(8,10))
-        return new Date(year, month - 1, day, 0, 0, 0, 0)
-    }
-
     render() {
         let defaultDate = new Date()
-        if (this.state.events.length !== 0){
-            const dateString = this.state.events.reduce(function (r, a) {
-                return r.start > a.start ? r.start : a.start;
-            })
-          
-            defaultDate = this.parseDate(dateString)
-        }
-
         this.state.events.forEach(function(item, index) {
             item.start = new Date(item.start)
             item.end = new Date(item.end)
         })
+        
+        if (this.state.events.length !== 0){
+            defaultDate = this.state.events.reduce(function (r, a) {
+                return r.start > a.start ? r.start : a.start;
+            })
+        }
+
+        
         return (
             <div className="Calendar">
             <Calendar
